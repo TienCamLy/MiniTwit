dc30d6ec1fb9546148e846e01991df8f07ac3ada
Mie Jonasson <jonasson2001@gmail.com>
2026-01-30 22:00:51 +0100

    Fix: Add read mode (as opposed to default read-binary mode) to reading of SQL file

--- minitwit.py
+++ minitwit.py
@@ -46,7 +46,7 @@ def connect_db():
 def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
+        with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
