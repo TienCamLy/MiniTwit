60c48eb3fb7bb5835363a5c5b61645343128db99
ptso <ptso@itu.dk>
2026-02-06 14:56:36 +0100

    Added razor pages template

---
+++ razor-pages/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_pages.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
