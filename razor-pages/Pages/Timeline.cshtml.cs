6d0571057c20851aecb1bf5d5174955ee4edfcb9
tienl <tienly197@gmail.com>
2026-02-08 17:01:15 +0100

    Public timeline shows messages

--- razor-pages/Pages/Timeline.cshtml.cs
+++ razor-pages/Pages/Timeline.cshtml.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using razor_pages.Structs;
+using System.Globalization;
 
 namespace razor_pages.Pages;
 
 public class TimelineModel : PageModel
 {
+    public IEnumerable<Message> Messages { get; set; } = new List<Message>();
+
     private readonly IDBContext _dbcontext;
     public TimelineModel(IDBContext dbcontext)
     {
@@ -12,5 +16,17 @@ public class TimelineModel : PageModel
     }
     public void OnGet()
     {
+        Messages = _dbcontext.GetPublicTimeline(30);
+
+        foreach (var message in Messages) // temporary solution
+        {
+            DateTime publishedAt =
+                DateTimeOffset.FromUnixTimeSeconds(long.Parse(message.pub_date)).DateTime;
+            
+            string formattedTime = publishedAt.ToString("yyyy-MM-dd @ HH:mm");
+            
+            message.pub_date = formattedTime;
+
+        }
     }
 }
