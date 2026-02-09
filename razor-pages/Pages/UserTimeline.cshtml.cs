ee0bdd524fdc3551ae12351a470ac72134e20e61
Mie Jonasson <jonasson2001@gmail.com>
2026-02-09 10:31:37 +0100

    add functionality of user timeline initialization and posting of follow / unfollow - note that the db update and page reload on follow / unfollow is not working and reloads an empty person-page even though the url is correct.

--- razor-pages/Pages/UserTimeline.cshtml.cs
+++ razor-pages/Pages/UserTimeline.cshtml.cs
@@ -1,16 +1,41 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using razor_pages.Structs;
 
 namespace razor_pages.Pages;
 
 public class UserTimelineModel : PageModel
 {
+    public IEnumerable<Message> Messages { get; set; } = new List<Message>();
+    public string Username { get; set; } = string.Empty;
+    public bool Followed { get; set; } = false;
+
     private readonly IDBContext _dbcontext;
+    
     public UserTimelineModel(IDBContext dbcontext)
     {
         _dbcontext = dbcontext;
     }
-    public void OnGet()
+    
+    public void OnGet(string user)
     {
+        Messages = _dbcontext.GetUserTimeline(30, user);
+        Username = user;
+        Followed = User.Identity?.Name != null && _dbcontext.IsFollowed(User.Identity.Name, user);
+    }
+    
+    public IActionResult OnPost()
+    {
+        if (Followed)
+        {
+            Followed = false;
+        }
+        else
+        {
+            Followed = true;
+        }
+        
+        return RedirectToPage("/Public", new { username = Username });
     }
 }
