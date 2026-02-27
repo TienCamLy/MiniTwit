2fd808fa03d5295a5db02e2a8a7c86509678a59a
ptso <ptso@itu.dk>
2026-02-27 15:11:48 +0100

    Restructured MyTimeline

--- razor-pages/Web/Pages/MyTimeline.cshtml.cs
+++ razor-pages/Web/Pages/MyTimeline.cshtml.cs
@@ -1,33 +1,41 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using razor_pages.Structs;
+using Core.DTOs;
 
-namespace razor_pages.Pages;
+namespace Web.Pages;
 
 public class MyTimelineModel : PageModel
 {
+    private readonly IMessageRepository _messageRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IFollowerRepository _followerRepository;
+    
     public IEnumerable<Message> Messages { get; set; } = new List<Message>();
     [BindProperty]
     public string Text { get; set; } = "";
-
-    private readonly IDBContext _dbcontext;
     private int UserId => int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
-    public MyTimelineModel(IDBContext dbcontext)
+    
+    public MyTimelineModel(
+        MessageRepository messageRepository, 
+        UserRepository userRepository, 
+        FollowerRepository followerRepository)
     {
-        _dbcontext = dbcontext;
+        _messageRepository = messageRepository;
+        _userRepository = userRepository;
+        _followerRepository = followerRepository;
     }
     public IActionResult OnGet()
     {
         if (User.Identity?.IsAuthenticated != true || User.Identity.Name == null) return Redirect("/public");
         
-        Messages = _dbcontext.GetOwnTimeline(30, UserId);
+        Messages = _messageRepository.GetOwnTimeline(UserId);
         return Page();
     }
     
     public IActionResult OnPostCreateMessage()
     {
-        _dbcontext.CreateMessage(UserId, Text);
+        _messageRepository.CreateMessage(UserId, Text);
         TempData["FlashMessage"] = "Your message was recorded";
         return RedirectToPage();
     }
--- razor-pages/Web/Pages/Privacy.cshtml.cs
+++ razor-pages/Web/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace razor_pages.Pages;
+namespace Web.Pages;
 
 public class PrivacyModel : PageModel
 {
