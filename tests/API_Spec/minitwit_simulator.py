ca28f0bab90d1309a864fc80564971cb687dfa0c
Mie Jonasson <jonasson2001@gmail.com>
2026-03-21 01:37:25 +0100

    remove debug print

--- tests/API_Spec/minitwit_simulator.py
+++ tests/API_Spec/minitwit_simulator.py
@@ -96,9 +96,8 @@ def main(host, token, max_actions=None):
     HEADERS = {
         "Connection": "close",
         "Content-Type": "application/json",
-        f"Authorization": f"Basic {token}",
+        "Authorization": f"Basic {token}",
     }
-    print(f"Headers: {HEADERS}")
     total_actions = 0
     for action, delay in get_actions():
         if max_actions and total_actions >= max_actions:
