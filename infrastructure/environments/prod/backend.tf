b1d5a602e7954ec893d28947c6cc9a13f7cb1701
Mie Jonasson <jonasson2001@gmail.com>
2026-05-08 14:24:03 +0200

    Get Cursor's help to fix backend config

--- infrastructure/environments/prod/backend.tf
+++ infrastructure/environments/prod/backend.tf
@@ -1,11 +1,16 @@
 terraform {
   backend "s3" {
-    region                      = "fra1"
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+
+    region                      = "us-east-1"
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_s3_checksum            = true
     acl                         = "private"
-    endpoint                    = "https://fra1.digitaloceanspaces.com"
     force_path_style            = true
   }
 }
